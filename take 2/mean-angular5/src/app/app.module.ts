7aaba5fc92535e5fe8550d614f1b841b6ca02450
piratezilla <tralockrox@gmail.com>
2019-06-12 14:46:05 +0100

    step8

--- take 2/mean-angular5/src/app/app.module.ts
+++ take 2/mean-angular5/src/app/app.module.ts
@@ -7,12 +7,24 @@ import { BookComponent } from './book/book.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
 
 const appRoutes: Routes = [
   {
     path: 'books',
     component: BookComponent,
     data: { title: 'Book List' }
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailComponent,
+    data: { title: 'Book Details' }
+  },
+  {
+    path: 'book-create',
+    component: BookCreateComponent,
+    data: { title: 'Create Book' }
   },
   { path: '',
     redirectTo: '/books',
@@ -22,7 +34,9 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent
   ],
   imports: [
     BrowserModule,
