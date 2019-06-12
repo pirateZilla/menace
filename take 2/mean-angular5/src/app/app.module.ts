5bcca4e63d3acb99ac9e5a6c95c2ea3d96deb229
piratezilla <tralockrox@gmail.com>
2019-06-12 14:01:43 +0100

    step6

--- take 2/mean-angular5/src/app/app.module.ts
+++ take 2/mean-angular5/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
